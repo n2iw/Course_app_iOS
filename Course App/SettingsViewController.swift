3f069251bec45addfa6c67dff95b50b79b92cb41
Ming Ying <yingming18@gmail.com>
2016-10-29 01:35:07 -0400

    Let user enter first and last name, and save them

--- Course App/SettingsViewController.swift
+++ Course App/SettingsViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITextFieldDelegate {
+    
+    let FIRST_NAME = "firstName"
+    let LAST_NAME = "lastName"
 
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    private let defaults = NSUserDefaults.standardUserDefaults()
+    private var firstName: String?;
+    private var lastName: String?;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        firstName = defaults.stringForKey(FIRST_NAME)
+        lastName = defaults.stringForKey(LAST_NAME)
+        firstNameField.text = firstName
+        lastNameField.text = lastName
 
-        // Do any additional setup after loading the view.
+        saveButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +36,49 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveNames(sender: UIButton) {
+        if let first_name = firstNameField.text{
+            if first_name != self.firstName {
+                firstName = first_name.capitalizedString
+                defaults.setObject( firstName, forKey: FIRST_NAME)
+                firstNameField.text = firstName
+            }
+        }
+        if let last_name = lastNameField.text {
+            if last_name != self.lastName {
+                lastName = last_name.capitalizedString
+                defaults.setObject( lastName, forKey: LAST_NAME)
+                lastNameField.text = lastName
+            }
+        }
+        defaults.synchronize()
+        saveButton.enabled = false
+        firstNameField.resignFirstResponder()
+        lastNameField.resignFirstResponder()
     }
-    */
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        saveButton.enabled = false
+        if let first_name = firstNameField.text{
+            if first_name != self.firstName {
+                saveButton.enabled = true
+            }
+        }
+        
+        if let last_name = lastNameField.text {
+            if last_name != self.lastName {
+                saveButton.enabled = true
+            }
+        }
+    }
 
 }
