f1164d8bbdee97d32459777ed963ea6d6d27fbef
Ming Ying <yingming18@gmail.com>
2016-11-20 01:04:55 -0500

    Fix Datetime cover error

--- Course App/JSONDate.swift
+++ Course App/JSONDate.swift
@@ -11,7 +11,7 @@ import Foundation
 class JSONDate {
     class func dateFromJSONString(dateString: String) -> NSDate? {
         let formater = NSDateFormatter()
-        formater.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        formater.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
         
         return formater.dateFromString(dateString)
     }
